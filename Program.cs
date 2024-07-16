d30167586469148254b886825bdbf7ccee4ee064
Sultan Galiullin <sultangal@outlook.com>
2024-07-16 14:16:16 +0300

    Глава 21

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
-// using System.Text.Json.Serialization;
-using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 using WebApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,61 +8,14 @@ builder.Services.AddDbContext<DataContext>(opts =>
         "ConnectionStrings:ProductConnection"]);
     opts.EnableSensitiveDataLogging(true);
 });
+builder.Services.AddControllersWithViews();
 
-builder.Services.AddControllers()
-    .AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
-
-builder.Services.AddRateLimiter(opts =>
-{
-    opts.AddFixedWindowLimiter("fixedWindow", fixOpts =>
-    {
-        fixOpts.PermitLimit = 1;
-        fixOpts.QueueLimit = 0;
-        fixOpts.Window = TimeSpan.FromSeconds(15);
-    });
-});
-
-// builder.Services.Configure<JsonOptions>(opts => {
-//     opts.JsonSerializerOptions.DefaultIgnoreCondition 
-//         = JsonIgnoreCondition.WhenWritingNull;
-// });
-
-builder.Services.Configure<MvcNewtonsoftJsonOptions>(opts =>
-{
-    opts.SerializerSettings.NullValueHandling
-        = Newtonsoft.Json.NullValueHandling.Ignore;
-});
-
-builder.Services.Configure<MvcOptions>(opts =>
-{
-    opts.RespectBrowserAcceptHeader = true;
-    opts.ReturnHttpNotAcceptable = true;
-});
-
-builder.Services.AddOutputCache(opts =>
-{
-    opts.AddPolicy("30sec", policy =>
-    {
-        policy.Cache();
-        policy.Expire(TimeSpan.FromSeconds(30));
-    });
-});
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "WebApp", Version = "v1"
-    });
-});
 var app = builder.Build();
-app.UseRateLimiter();
-app.UseOutputCache();
+
+app.UseStaticFiles();
 app.MapControllers();
-app.MapGet("/", () => "Hello World!");
-app.UseSwagger();
-app.UseSwaggerUI(options => { options.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApp"); });
+app.MapDefaultControllerRoute();
 var context = app.Services.CreateScope().ServiceProvider
     .GetRequiredService<DataContext>();
 SeedData.SeedDatabase(context);
-
 app.Run();
