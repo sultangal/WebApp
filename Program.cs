461b43ebb1e46d097aa3ac657da5309764d125e9
Sultan Galiullin <sultangal@outlook.com>
2024-07-20 00:15:09 +0300

    Добавил libman в репо

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
 
@@ -10,30 +9,14 @@ builder.Services.AddDbContext<DataContext>(opts =>
     opts.EnableSensitiveDataLogging(true);
 });
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddRazorPages();
-
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession(options => {
-    options.Cookie.IsEssential = true;
-});
-
-builder.Services.Configure<RazorPagesOptions>(opts => {
-    opts.Conventions.AddPageRoute("/Index", "/extra/page/{id:long?}");
-});
-
 builder.Services.AddSingleton<CitiesData>();
-
 var app = builder.Build();
-
 app.UseStaticFiles();
-app.UseSession();
 app.MapControllers();
 app.MapDefaultControllerRoute();
 app.MapRazorPages();
-
 var context = app.Services.CreateScope().ServiceProvider
     .GetRequiredService<DataContext>();
 SeedData.SeedDatabase(context);
-
-app.Run();
+app.Run();  
